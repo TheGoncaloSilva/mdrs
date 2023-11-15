74a8f1b0a7481f11971297486d4076a4657dfc57
goncalo <goncalolsilva@ua.pt>
2023-11-15 11:53:45 +0000

    finish exercise 8

---
+++ practical/class_1/Task8_MATLABcodes/greedyRandomizedStrategy.m
@@ -0,0 +1,32 @@
+function [bestSol, bestLoads, bestLoad, contador, somador] = greedyRandomizedStrategy(sP, nSP, T, nNodes, Links, timeLimit)
+    nFlows = height(T);
+    t= tic;
+    bestLoad= inf;  % objective function value
+    contador= 0;
+    somador= 0;
+    while toc(t) < timeLimit
+        sol= zeros(1,nFlows);
+        for f= randperm(nFlows)
+            auxBest= inf;
+            for i=1:nSP(f)
+                sol(f)= i;
+                auxLoads= calculateLinkLoads(nNodes,Links,T,sP,sol);
+                auxLoad= max(max(auxLoads(:,3:4)));
+                if auxLoad < auxBest
+                    auxBest= auxLoad;
+                    ibest=i;
+                end
+            end
+            sol(f)= ibest;
+        end
+        Loads= calculateLinkLoads(nNodes,Links,T,sP,sol);
+        load= max(max(Loads(:,3:4)));   % calculate max capacity of link loads
+        if load<bestLoad
+            bestSol= sol;
+            bestLoad= load;
+            bestLoads= Loads;
+        end
+        contador= contador+1;
+        somador= somador+load;
+    end
+end
--- practical/class_1/Task8_MATLABcodes/task_8.m
+++ practical/class_1/Task8_MATLABcodes/task_8.m
@@ -43,7 +43,7 @@ maxLoad= max(max(Loads(:,3:4)));
 
 %Optimization algorithm based on random strategy:
 t= tic;
-timeLimit= 5;
+timeLimit= 5;   % in seconds
 bestLoad= inf;  % objective function value
 contador= 0;
 somador= 0;
@@ -89,3 +89,157 @@ end
 %   algorithm. Compare the worst link load values of this solution and the solution of 8.b.
 fprintf('\nExercise 8.d:\n');
 
+% Computing up to k=6 shortest paths for all flows from 1 to nFlows:
+k= inf;
+sP= cell(1,nFlows);
+nSP= zeros(1,nFlows);
+for f=1:nFlows
+    [shortestPath, totalCost] = kShortestPath(L,T(f,1),T(f,2),k);
+    sP{f}= shortestPath;
+    nSP(f)= length(totalCost);
+end
+
+% Compute the link loads using the first (shortest) path of each flow:
+sol= ones(1,nFlows);
+Loads= calculateLinkLoads(nNodes,Links,T,sP,sol);
+% Determine the worst link load:
+maxLoad= max(max(Loads(:,3:4)));
+
+%Optimization algorithm based on random strategy:
+t= tic;
+timeLimit= 5;   % in seconds
+bestLoad= inf;  % objective function value
+contador= 0;
+somador= 0;
+while toc(t) < timeLimit
+    sol= zeros(1,nFlows);
+    for f= 1:nFlows
+        sol(f)= randi(nSP(f));
+    end
+    Loads= calculateLinkLoads(nNodes,Links,T,sP,sol);
+    load= max(max(Loads(:,3:4)));   % calculate max capacity of link loads
+    if load<bestLoad
+        bestSol= sol;
+        bestLoad= load;
+        bestLoads= Loads;
+    end
+    contador= contador+1;
+    somador= somador+load;
+end
+%Output of link loads of the routing solution:
+fprintf('Worst link load of the best solution = %.2f\n',bestLoad);
+fprintf('Link loads of the best solution:\n')
+for i= 1:nLinks
+    fprintf('{%d-%d}:\t%.2f\t%.2f\n',bestLoads(i,1),bestLoads(i,2),bestLoads(i,3),bestLoads(i,4))
+end
+
+% Conclsuion: The link load is better in this exercise because the random
+%   strategy is selecting from all available paths (k=inf) whereas before 
+%   was selecting from only one path (k=1)
+
+%% Exercise 8.e - Repeat task 8.d but now considering only 6 candidate routing 
+%   paths for each flow. Compare these results with the ones obtained in task 8.d.
+fprintf('\nExercise 8.e:\n');
+
+% Computing up to k=6 shortest paths for all flows from 1 to nFlows:
+k= 6;
+sP= cell(1,nFlows);
+nSP= zeros(1,nFlows);
+for f=1:nFlows
+    [shortestPath, totalCost] = kShortestPath(L,T(f,1),T(f,2),k);
+    sP{f}= shortestPath;
+    nSP(f)= length(totalCost);
+end
+
+% Compute the link loads using the first (shortest) path of each flow:
+sol= ones(1,nFlows);
+Loads= calculateLinkLoads(nNodes,Links,T,sP,sol);
+% Determine the worst link load:
+maxLoad= max(max(Loads(:,3:4)));
+
+%Optimization algorithm based on random strategy:
+t= tic;
+timeLimit= 5;   % in seconds
+bestLoad= inf;  % objective function value
+contador= 0;
+somador= 0;
+while toc(t) < timeLimit
+    sol= zeros(1,nFlows);
+    for f= 1:nFlows
+        sol(f)= randi(nSP(f));
+    end
+    Loads= calculateLinkLoads(nNodes,Links,T,sP,sol);
+    load= max(max(Loads(:,3:4)));   % calculate max capacity of link loads
+    if load<bestLoad
+        bestSol= sol;
+        bestLoad= load;
+        bestLoads= Loads;
+    end
+    contador= contador+1;
+    somador= somador+load;
+end
+
+%Output of link loads of the routing solution:
+fprintf('Worst link load of the best solution = %.2f\n',bestLoad);
+fprintf('Solutions considered: %d\n', contador);
+fprintf('Avg. worst link load among all solutions= %.2f\n',somador/contador);
+
+% Conclusion: Limiting the k=6 will force the program to use the same 5
+%   seconds to focus on a region of possible best solutions. This doesn't
+%   mean that we are achieving the best possible solution, but in this way
+%   we improve overall solutions.
+
+%% Exercise 8.f - Use the provided optimization algorithm (based on the 
+%   random strategy) to develop an optimization algorithm based on a greedy 
+%   randomized strategy -> done in function greedyRandomizedStrategy
+
+%% Exercise 8.g - Run the optimization algorithm based on the greedy 
+%   randomized strategy (developed in 8.f) with a runtime limit of 5 
+%   seconds and considering all possible routing paths for each flow. 
+%   Compare these results with the ones obtained in tasks 8.d and 8.e.
+fprintf('\nExercise 8.g:\n');
+
+% Computing up to k=6 shortest paths for all flows from 1 to nFlows:
+k= inf;
+sP= cell(1,nFlows);
+nSP= zeros(1,nFlows);
+for f=1:nFlows
+    [shortestPath, totalCost] = kShortestPath(L,T(f,1),T(f,2),k);
+    sP{f}= shortestPath;
+    nSP(f)= length(totalCost);
+end
+
+[bestSol, bestLoads, bestLoad, contador, somador] = greedyRandomizedStrategy(sP, nSP, T, nNodes, Links, 5);
+
+%Output of link loads of the routing solution:
+fprintf('Worst link load of the best solution = %.2f\n',bestLoad);
+fprintf('Solutions considered: %d\n', contador);
+fprintf('Avg. worst link load among all solutions= %.2f\n',somador/contador);
+
+% Conclusion: Althought the Worst link load didn't improve, the greedy 
+%   randomized strategy analized fewer solutions, but outputted best avg
+%   worst link load among all solutions, indicating that having more
+%   computing power and reducing the time limit, could lead to generating 
+%   equal or better solutions than the complete randomized approach 
+
+%% Exercise 8.h - Repeat task 8.g but now considering only 6 candidate 
+%   routing paths for each flow. Compare these results with the ones 
+%   obtained in tasks 8.d, 8.e and 8.g.
+fprintf('\nExercise 8.h:\n');
+
+% Computing up to k=6 shortest paths for all flows from 1 to nFlows:
+k= 6;
+sP= cell(1,nFlows);
+nSP= zeros(1,nFlows);
+for f=1:nFlows
+    [shortestPath, totalCost] = kShortestPath(L,T(f,1),T(f,2),k);
+    sP{f}= shortestPath;
+    nSP(f)= length(totalCost);
+end
+
+[bestSol, bestLoads, bestLoad, contador, somador] = greedyRandomizedStrategy(sP, nSP, T, nNodes, Links, 5);
+
+%Output of link loads of the routing solution:
+fprintf('Worst link load of the best solution = %.2f\n',bestLoad);
+fprintf('Solutions considered: %d\n', contador);
+fprintf('Avg. worst link load among all solutions= %.2f\n',somador/contador);
