e7913b60e6dd1eac6f8aa7de6d3c3bf988c31b9d
goncalo <goncalolsilva@ua.pt>
2023-12-18 22:42:20 +0000

    task 12 finished

--- practical/class_1/task12/task12.m
+++ practical/class_1/task12/task12.m
@@ -11,7 +11,6 @@ clear
 load('InputData2.mat')
 nNodes= size(Nodes,1);
 nLinks= size(Links,1);
-nFlows= size(T,1);
 
 fprintf('Exercise 12.a:\n');
 
@@ -35,32 +34,9 @@ fprintf("W = %.2f ms A = %.2f ms\n\n", worstCost * 2 * 1000, mean(costs) * 2 * 1
 
 %% Exercise 12.b - Consider again that the anycast nodes are node 3 and 
 %   node 5. Determine the link loads of all links and the worst link load. 
-
 fprintf('Exercise 12.b:\n');
 
-T = zeros(height(sourceNodes), 4);
-idx = 1;
-
-% Iterate over paths
-for p = sP
-    if isempty(p{1})
-        continue;
-    end
-
-    srcN = p{1}{1}(1);
-    dstN = p{1}{1}(end);
-    T(idx, 1) = srcN;
-    T(idx, 2) = dstN;
-    T(idx, 3) = throughput(2, srcN);
-    T(idx, 4) = throughput(1, srcN);
-
-    idx = idx + 1;
-end
-
-sol = ones(1, nNodes-length(anycastNodes));
-sP = sP(~cellfun(@isempty, sP));    % remove empty entry from the cell array
-
-Loads = calculateLinkLoads(nNodes, Links, T, sP, sol);
+Loads= determine_solution(sourceNodes, sP, nNodes, Links, throughput, anycastNodes);
 
 % Determine the worst link load:
 maxLoad= max(max(Loads(:,3:4)));
@@ -78,6 +54,135 @@ end
 %   worst link load of the best solution. Compare these results with the 
 %   results obtained for the anycast nodes 3 and 5 and take conclusions.
 
-fprintf('Exercise 12.c:\n');
+fprintf('\nExercise 12.c:\n');
+clear
+
+load('InputData2.mat')
+nNodes= size(Nodes,1);
+
+% Input data
+v= 2*10^5;    % km/sec
+D= L/v;
+throughput = [0.7 1.5 2.1 1.0 1.3 2.7 2.2 0.8 1.7 1.9 2.8;
+              0.1 0.2 0.3 0.1 0.1 0.4 0.3 0.1 0.2 0.3 0.4];
+anycastNodes = [3 5];
+combs = nchoosek(1:11, 2);  % All combinations of anycastNodes
+
+% Initialization
+bestWorstCost = inf;
+bestAnycastNodes = combs(1, :);
+bestSP = cell(1, nNodes);
+k = 1; % Number of paths for each node
+
+% Loop through combinations
+for i = 1:size(combs, 1)
+    anycastNodes = combs(i, :);
+    sourceNodes = setdiff(1:nNodes, anycastNodes);
+    
+    % Optimization step
+    [costs, sP] = optimizePath(nNodes, anycastNodes, D, k);
+    [worstCost, ~] = max(costs);
+
+    % Update best values if better result found
+    if worstCost < bestWorstCost
+        bestWorstCost = worstCost;
+        bestAnycastNodes = anycastNodes;
+        bestSP = sP;
+    end
+end
 
 
+sP= bestSP;
+Loads= determine_solution(sourceNodes, sP, nNodes, Links, throughput, anycastNodes);
+
+% Determine the worst link load:
+maxLoad= max(max(Loads(:,3:4)));
+
+fprintf("Anycast Nodes = %s\n", num2str(bestAnycastNodes));
+fprintf("Worst round-trip delay = %.2f ms\n", bestWorstCost * 2 * 1000);
+fprintf("Worst link load = %.2f Gbps\n", maxLoad);
+
+
+%% Exercise 12.d - Again, consider that you can freely select the nodes to 
+%   connect the two DCs running the service. Try all possible combinations 
+%   of 2 nodes and select the one that minimizes the worst link load. 
+%   Indicate the two selected nodes, the worst round-trip delay, and the 
+%   worst link load of the best solution. Compare these results with all 
+%   previous results and take conclusions.
+
+fprintf('\nExercise 12.d:\n');
+clear
+
+load('InputData2.mat')
+nNodes= size(Nodes,1);
+nFlows= size(T,1);
+
+% Input data
+v= 2*10^5;    % km/sec
+D= L/v;
+throughput= [0.7 1.5 2.1 1.0 1.3 2.7 2.2 0.8 1.7 1.9 2.8;
+              0.1 0.2 0.3 0.1 0.1 0.4 0.3 0.1 0.2 0.3 0.4];
+anycastNodes= [3 5];
+combs= nchoosek(1:11, 2);  % All combinations of anycastNodes
+
+% Initialization
+bestCosts= inf;
+bestMaxLoad= inf;
+bestAnycastNodes= zeros(1, 2);
+bestSP= cell(1, nNodes);
+k= 1; % Number of paths for each node
+
+% Loop through combinations
+for i = 1:size(combs, 1)
+    anycastNodes= combs(i, :);
+    sourceNodes= setdiff(1:nNodes, anycastNodes);
+    
+    % Optimization step
+    [costs, sP]= optimizePath(nNodes, anycastNodes, D, k);
+
+    Loads= determine_solution(sourceNodes, sP, nNodes, Links, throughput, anycastNodes);
+    
+    % Determine the worst link load:
+    maxLoad= max(max(Loads(:,3:4)));
+
+    % Update best values if better result found
+    if maxLoad < bestMaxLoad
+        bestMaxLoad = maxLoad;
+        bestAnycastNodes = anycastNodes;
+        bestCosts = costs;
+    end
+end
+
+[worstCost, worstNode] = max(bestCosts);
+
+fprintf("Anycast Nodes = %s\n", num2str(bestAnycastNodes));
+fprintf("Worst round-trip delay = %.2f ms\n", worstCost * 2 * 1000);
+fprintf("Worst link load = %.2f Gbps\n", bestMaxLoad);
+
+
+%% Determine Solution function
+function Loads=determine_solution(sourceNodes, sP, nNodes, Links, throughput, anycastNodes)
+    T = zeros(height(sourceNodes), 4);
+    idx = 1;
+    
+    % Iterate over paths
+    for p = sP
+        if isempty(p{1})
+            continue;
+        end
+    
+        srcN = p{1}{1}(1);
+        dstN = p{1}{1}(end);
+        T(idx, 1) = srcN;
+        T(idx, 2) = dstN;
+        T(idx, 3) = throughput(2, srcN);
+        T(idx, 4) = throughput(1, srcN);
+    
+        idx = idx + 1;
+    end
+    
+    sol = ones(1, nNodes-length(anycastNodes));
+    sP = sP(~cellfun(@isempty, sP));    % remove empty entry from the cell array
+    
+    Loads = calculateLinkLoads(nNodes, Links, T, sP, sol);
+end
